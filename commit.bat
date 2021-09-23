402b0cf09fbadd661a8799f12ab0c892c6cdea38
uni-shpark <suhyung007@gmail.com>
2021-09-23 14:35:25 +0900

    commit

---
+++ commit.bat
@@ -0,0 +1,6 @@
+CALL copy E:\workspace_cloudia\tomcat-sellers\WebContent\WEB-INF\classes\com\uni\sellers\clientsalesactive\ClientSalesActiveController.class E:\thesellers\tmp\ROOT\WEB-INF\classes\com\uni\sellers\clientsalesactive
+
+CALL git add .
+CALL git commit -m "commit"
+CALL git branch -M master
+CALL git push -u origin master
